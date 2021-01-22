92f33faeb6e399c6fb4932f76c33790cb44d83cb
Ihor Vodko <ihorfinance@gmail.com>
2021-01-22 14:19:18 +0200

    added const MainThread

--- DirAnalyzer.hpp
+++ DirAnalyzer.hpp
@@ -1,1 +1,37 @@
+#pragma once
+#include <mutex>
+#include <thread>
+#include <vector>
+#include "FileAnalyzer.h"
+using namespace std;
 
+const size_t MainThread = 1;
+
+//class implement concurrent analysis of files (*.h, *.hpp, *.c, *.cpp) 
+//which pathes are stored in the vector
+class DirAnalyzer {
+private:
+	filesystem::path dirPath;
+	vector<FileAnalyzer> vFiles;
+	mutex dirAnalyzerMutex;
+	int TotalBlankLinesCount{};
+	int	TotalCommentedLinesCount{};
+	int	TotalCodeLinesCount{};
+	int	TotalPhysicalLinesCount{};
+	int	TotalProcessedFilesCount{};
+
+	bool IsCorrectExtension(const filesystem::path&);
+	void PerformDirAnalysis(size_t, size_t);
+public:
+	int GetTotalBlankLinesCount() const;
+	int GetTotalCommentedLinesCount() const;
+	int GetTotalCodeLinesCount() const;
+	int GetTotalPhysicalLinesCount() const;
+	int GetProcessedFilesCount() const;
+	void SetDirPath(const filesystem::path& dirPath);
+	void AnalyzeDir();
+	void PrintDirAnalysis(ostream&) const;
+
+	DirAnalyzer() = default;
+	DirAnalyzer(const filesystem::path&);
+};
